2b21aa174d66200c20390e0ba1307bb7a7859a36
Christian Aziaka <christian.aziaka@epita.fr>
2025-11-28 09:45:02 +0100

    first push, project upload

---
+++ src/main/java/org/formatoion/projet_aziaka_christian/ProjetAziakaChristianApplication.java
@@ -0,0 +1,13 @@
+package org.formatoion.projet_aziaka_christian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAziakaChristianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetAziakaChristianApplication.class, args);
+    }
+
+}
